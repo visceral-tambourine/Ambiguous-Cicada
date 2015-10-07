6e055e849d56a347817d0c742a402ab6d194b47e
Phong Pham <phizzy@gmail.com>
2015-10-07 12:31:42 -0700

    Implement geolocation
    Add location-load.html

--- client-mobile/www/js/match.js
+++ client-mobile/www/js/match.js
@@ -1,40 +1,63 @@
 angular.module('kwiki.match', [])
 
-.factory('MatchFactory', ['$state', 'SocketFactory', '$window', '$rootScope', function ($state, SocketFactory, $window, $rootScope) {
-  var matchFact = {};
 
-  matchFact.connectSocket = function () {
-    this.socket = SocketFactory.connect("match");
+//this will be where user picks the restaurant instead
+
+// new restaurant logic
+                                         
+.factory('MatchFactory', ['$state', 'SocketFactory', '$window', '$rootScope', 
+  function ($state, SocketFactory, $window, $rootScope) {
+    var matchFact = {};
+
+    matchFact.connectSocket = function () {
+      this.socket = SocketFactory.connect("match");
   };
 
-  matchFact.postMatch = function () {
-    this.socket.emit('matching', $rootScope.user);
-    this.socket.on('matched', function (data) {
-      $rootScope.chatRoomId = data;
-      $rootScope.$apply(function () {
-        $state.go('chat');
+    matchFact.postMatch = function () {
+      this.socket.emit('matching', $rootScope.user);
+      this.socket.on('matched', function (data) {
+        $rootScope.chatRoomId = data;
+        $rootScope.$apply(function () {
+          $state.go('chat');
+        });
       });
-    });
   };
 
   return matchFact;
 }])
 
-.controller('MatchCtrl', ['$rootScope', '$state', '$scope', 'MatchFactory', 'AuthFactory', function ($rootScope, $state, $scope, MatchFactory, AuthFactory) {
-  $rootScope.disableButton = false;
-
-  $scope.connect = function() {
-    MatchFactory.connectSocket();
-  };
-
-  $scope.submit = function () {
-    $rootScope.disableButton = true;
-    MatchFactory.postMatch();
-    $state.go('load');
-  };
-
-  $scope.logOut = function () {
+.controller('MatchCtrl', ['$rootScope', '$state', '$scope', 'MatchFactory', 'AuthFactory', '$window', 
+  function ($rootScope, $state, $scope, MatchFactory, AuthFactory, $window) {
     $rootScope.disableButton = false;
-    AuthFactory.logOut();
-  };
+    $scope.locationLoad;
+
+    $scope.getLocation = function() {
+      if ('geolocation' in $window.navigator) {
+        $window.navigator.geolocation.getCurrentPosition(function(position) {
+          $rootScope.user.position = position;
+          MatchFactory.postMatch();
+          console.log('GETTING POSITION', position);
+        });
+      }
+    };
+
+    $scope.connect = function() {
+      MatchFactory.connectSocket();
+    };
+
+    // $scope.submit = function () {
+    //   $rootScope.disableButton = true;
+    //   MatchFactory.postMatch();
+    //   $state.go('load');
+    // };
+
+    $scope.logOut = function () {
+      $rootScope.disableButton = false;
+      AuthFactory.logOut();
+    };
+
+    if ($state.current.name === 'location-load') {
+      $scope.getLocation();
+      $scope.locationLoad = false;
+    }
 }]);
